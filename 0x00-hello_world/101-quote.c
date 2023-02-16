b080966186d823d45754c77fb77ab7c2ee85c356
Modeste <ciiramodeste23@gmail.com>
2023-02-16 12:42:30 -0800

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 56);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
