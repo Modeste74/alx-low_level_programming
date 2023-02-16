d339bb6c7e1768bea7ab8d5a7d8c38ce3a920202
Modeste <ciiramodeste23@gmail.com>
2023-02-16 12:06:08 -0800

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  * Return: 1 (Success)
  */
 int main(void)
+
 {
-	fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+return (1);
 }
