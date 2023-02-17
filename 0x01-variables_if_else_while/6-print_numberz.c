32ff9cfbe679b84108aaf6c99414897afcc9f0ed
Modeste <ciiramodeste23@gmail.com>
2023-02-17 10:18:08 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printing single digits with base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
