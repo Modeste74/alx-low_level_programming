8cdae3ba50017ba91d11f077d124ddfdc5d87149
Modeste <ciiramodeste23@gmail.com>
2023-02-17 11:28:36 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - code that prints all combinations of single digit.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 57; n++)
+		putchar(n);
+	if (n < 57)
+	{
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
