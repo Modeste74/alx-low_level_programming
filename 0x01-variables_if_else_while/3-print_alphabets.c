1f2569c8536be71599957f02d5b76ecfdc891d16
Modeste <ciiramodeste23@gmail.com>
2023-02-17 07:18:57 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - listing lowercase and then uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	for (d = 65; d <= 90; d++)
+		for (c = 97; c <= 122; c++)
+			putchar(c);
+	putchar(d);
+	putchar('\n');
+	return (0);
+}
