dea19198753aed6703149d772a0e578a076167aa
Modeste <ciiramodeste23@gmail.com>
2023-02-17 09:34:55 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,13 @@ int main(void)
 {
 	char c;
 
-	for (c = 97; c != 101 && c != 113 && c <= 122; c++)
-		putchar(c);
+	c = 97;
+		while (c <= 122)
+		{
+			if ((c != 113 && c != 101) && c <= 122)
+				putchar(c);
+			c++;
+		}
 	putchar('\n');
 	return (0);
 }
