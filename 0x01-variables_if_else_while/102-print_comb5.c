9724c0c714a853a4321c9cbad0822652e095446a
Modeste <ciiramodeste23@gmail.com>
2023-02-20 02:27:12 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - printing all combination of two two digits as per conditions
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, m, p, q;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			for (p = 48; p <= 57; p++)
+			{
+				for (q = 49; q <= 56; q++)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(' ');
+					putchar(p);
+					putchar(q);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
