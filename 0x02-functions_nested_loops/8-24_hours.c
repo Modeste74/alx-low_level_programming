bb7e679f97e601133965ded6d32e373f0a280143
Modeste <ciiramodeste23@gmail.com>
2023-02-21 11:58:04 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - used as prototype head
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int n, m, p, q;
+
+	for (n = 48; n <= 50; n++)
+	{
+		for (m = 48; m <= 51; m++)
+		{
+			for (p = 48; p <= 53; p++)
+			{
+				for (q = 48; q <= 57; q++)
+				{
+					_putchar(n);
+					_putchar(m);
+					_putchar(':');
+					_putchar(p);
+					_putchar(q);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
