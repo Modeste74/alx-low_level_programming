da502d17af7795a45cd64c8ffc9542427fd3e958
Modeste <ciiramodeste23@gmail.com>
2023-02-20 23:58:05 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	if (a < 5)
+	{
+		printf("_putchar\n");
+	}
+	else
+	{
+		printf("NOT\n");
+	}
+	return (0);
+}
+
