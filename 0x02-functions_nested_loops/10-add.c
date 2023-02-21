a9059c6dd6ac09d85e408860145304eb0686c43f
Modeste <ciiramodeste23@gmail.com>
2023-02-21 12:51:58 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - used as prototype head
+ * @n: number read
+ * @m: number read
+ * Return: result
+ */
+int add(int n, int m)
+{
+	int result = n + m;
+
+	return (result);
+}
+
