fb4622c7d52c07e0c6e1863a2dc930b1ea267b51
Modeste <ciiramodeste23@gmail.com>
2023-02-21 11:35:24 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prototype head
+ * @n: reads value of n
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int last_d = n % 10;
+
+	if (last_d < 0)
+	{
+		last_d = last_d * -1;
+	}
+	_putchar(last_d + 48);
+	return (last_d);
+}
