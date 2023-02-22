66117dc56a43f5fdd2f8274d1316146c33d47435
Modeste <ciiramodeste23@gmail.com>
2023-02-22 04:51:43 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,16 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n < 99)
+		while (n < 98)
 		printf("%d, ", n++);
+		printf("%d", n);
 		_putchar('\n');
 	}
 	else if (n >= 98)
 	{
-		while (n > 97)
+		while (n > 98)
 		printf("%d, ", n--);
+		printf("%d", n);
 		_putchar('\n');
 	}
 }
