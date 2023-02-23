08ae0dc3aadbe67a4bd26be5880c0720befe0a55
Modeste <ciiramodeste23@gmail.com>
2023-02-23 02:24:30 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - prototype head for this program.
+ *
+ * @c: to read and processed
+ * Return: 1 if upper and 0 otherwise
+ */
+int _isupper(int c)
+{
+	for (c = 65; c <= 90; c++)
+	{
+		return (1);
+	}
+	return (0);
+}
