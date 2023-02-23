eaa788c31b01d1934005b534e35049990ea7c474
Modeste <ciiramodeste23@gmail.com>
2023-02-23 11:04:33 -0800

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prototype head for the program
+ * @n: to be read
+ * REturn: void
+ */
+void print_diagonal(int n)
+{
+	int c, d;
+
+	for (c = 1; c <= n; c++)
+	{
+		for (d = 1; d <= c; d++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar(92);
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
