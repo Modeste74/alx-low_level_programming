af2ab4e7de2ad2868f915057d4c8fcd6705b45cd
Modeste <ciiramodeste23@gmail.com>
2023-02-23 22:34:55 -0800

    7-diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,18 +9,20 @@ void print_diagonal(int n)
 {
 	int c, d;
 
-	for (c = 1; c <= n; c++)
-	{
-		for (d = 1; d <= c; d++)
-		{
-			_putchar(' ');
-		}
-
-		_putchar('\\');
-		_putchar('\n');
-	}
 	if (n <= 0)
 	{
 		_putchar('\n');
+	}
+	else
+	{
+		for (c = 1; c <= n; c++)
+		{
+			for (d = 1; d <= c; d++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
