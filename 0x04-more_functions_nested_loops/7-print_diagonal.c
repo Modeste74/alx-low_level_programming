354dc28012a7f7f3102f8c8f92224a9036ea2069
Modeste <ciiramodeste23@gmail.com>
2023-02-23 22:57:40 -0800

    7-diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prototype head for the program
+ * @n: input to be read
+ * Return: diagonal
+ */
+void print_diagonal(int n)
+{
+	int c, d;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 1; c <= n; c++)
+		{
+			for (d = 1; d <= c; d++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
