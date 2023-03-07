7ad4cb1bfc5fc73f3a474e58de062f20a2a5f505
Modeste <ciiramodeste23@gmail.com>
2023-03-07 03:20:52 -0800

    done

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - prototype head for this function
+ * @s: used to set value
+ * @to: whose value is to be set
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
