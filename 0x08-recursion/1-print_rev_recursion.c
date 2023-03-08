4bba1393b53dc62860aa3782a3ce85732da9f794
Modeste <ciiramodeste23@gmail.com>
2023-03-08 01:11:54 -0800

    task done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - entry point
+ * @s: string to be printed in reverse order
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
