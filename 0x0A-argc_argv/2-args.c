74e1981e6b1cb0af5061a546480949b55e57d110
Modeste <ciiramodeste23@gmail.com>
2023-03-10 13:06:11 -0800

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @agrv: string of arguments
+ * @agrc: no of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int n = 0;
+
+	while (n < argc)
+	{
+		printf("%s\n", argv[n]);
+		n++;
+	}
+	return (0);
+}
