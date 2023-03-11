467a538c5309b7ba4868bd78acd3642ecdc71774
Modeste <ciiramodeste23@gmail.com>
2023-03-11 11:18:58 -0800

    done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argv: string
+ * @argc: number of arguements
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int n, sum = 0;
+
+	if (argc > 1)
+	{
+		for (n = 1; n < argc; n++)
+		{
+			if (*argv[n] < 48 || *argv[n] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[n]);
+		}
+		printf("%d\n", sum);
+	}
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	return (0);
+}
