b079ed39a3b70b6192f82928c195426148c9f57e
Modeste <ciiramodeste23@gmail.com>
2023-03-16 05:14:52 -0700

    task done

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - entry point
+ * @s1: first string
+ * @s2: string to be concat with the first
+ * @n: length of the second string
+ * Return: s
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int num1, num2, length, m;
+	char *s;
+
+	if (s1 == 0)
+	{
+		s1 = "";
+	}
+	if (s2 == 0)
+	{
+		s2 = "";
+	}
+
+	num1 = strlen(s1);
+	num2 = strlen(s2);
+
+	if (n >= num2)
+	{
+		length = num1 + num2;
+	}
+	else
+	{
+		length = num1 + n;
+	}
+
+	s = malloc((length + 1) * sizeof(char));
+
+	if (s == 0)
+	{
+		return (NULL);
+	}
+	for (m = 0; m <= num1; m++)
+	{
+		s[m] = s1[m];
+	}
+	for (m = 0; m < num2; m++)
+	{
+		s[num1 + m] = s2[m];
+	}
+	s[length + 1] = '\0';
+	return (s);
+}
