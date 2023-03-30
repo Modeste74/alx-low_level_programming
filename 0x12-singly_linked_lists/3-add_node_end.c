d152d3779acc5ddd2426c927b2b22d7d32d09be8
Modeste <ciiramodeste23@gmail.com>
2023-03-30 19:57:52 +0300

    Task done

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a node at the end of the list
+ * @head: pointer to a pointer
+ * @str: string to be duplicated
+ *
+ * Return: size of str
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *last, *following;
+
+	last = malloc(sizeof(list_t));
+	if (!last)
+	{
+		free(last);
+		return (NULL);
+	}
+	last->len = strlen(str);
+	last->str = strdup(str);
+	last->next = NULL;
+	
+	if (*head == NULL)
+	{
+		*head = last;
+	}
+	else
+	{
+		following = *head;
+		while (following->next != NULL)
+			{
+				following = following->next;
+			}
+		following->next = last;
+	}
+	return (*head);
+}
