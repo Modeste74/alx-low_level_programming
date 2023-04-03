810f64216cec6186d6d11a193b8485040fc1feec
Modeste <ciiramodeste23@gmail.com>
2023-04-03 19:53:19 +0300

    task done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: pointer that point to the head pointer of the list
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *store;
+
+	while (*head)
+	{
+		store = *head;
+		*head = (*head)->next;
+		free(store);
+	}
+	if (head == NULL)
+	{
+		return;
+	}
+}
