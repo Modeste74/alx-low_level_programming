5bae67b2e96c48bf803934edb07a981e5cbb5747
Modeste <ciiramodeste23@gmail.com>
2023-04-03 20:04:47 +0300

    task done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,9 +12,9 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		store = (*head)->next;
-		free(*head);
-		*head = store;
+		store = *head;
+		*head = (*head)->next;
+		free(store);
 	}
 	*head = NULL;
 }
