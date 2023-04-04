227dfb63df13572dbb3de0c4e5d5d54ed3bd6997
Modeste <ciiramodeste23@gmail.com>
2023-04-04 11:13:34 +0300

    task done

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums up all the data in the list
+ * @head: the beggining of the list
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int add;
+	
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != NULL)
+	{
+		add += head->n;
+		head = head->next;
+	}
+	return (add);
+}
+
