22673bfe784ccbe16d974f172649e1ad859531fd
Modeste <ciiramodeste23@gmail.com>
2023-04-10 22:38:16 +0300

    Task done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,22 @@ void error_msg(char *message, char *arg)
 {
 	dprintf(STDERR_FILENO, message, arg);
 }
+/**
+ * close_fd - displays error message for closing fail
+ * @fds: the file descriptors that fail to close
+ *
+ * Return: void
+ */
+void close_fd(int fds)
+{
+	int closing = close(fds);
 
+	if (closing == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fds);
+		exit(100);
+	}
+}
 /**
  * main - entry point
  * @argc: number of arguments passed
@@ -21,44 +36,44 @@ void error_msg(char *message, char *arg)
  *
  * Exit: exit success
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 	int fd_prev, fd_new, read_byte, written_byte;
 	char buffer[BUFF_SIZE];
 
-	if (argc != 3) {
+	if (argc != 3)
+	{
 		error_msg("Usage: %s file_from file_to\n", argv[0]);
-		error(97);
+		exit(97);
 	}
 	fd_prev = open(argv[1], O_RDONLY);
-	if (fd_prev == -1) {
+	if (fd_prev == -1)
+	{
 		error_msg("Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_new = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_new == -1) {
+	if (fd_new == -1)
+	{
 		error_msg("Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	read_byte = read(fd_prev, buffer, BUFF_SIZE);
-	while (read_byte > 0) {
+	while (read_byte > 0)
+	{
 		written_byte = write(fd_new, buffer, read_byte);
-		if (written_byte != read_byte) {
+		if (written_byte != read_byte)
+		{
 			error_msg("Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	if (read_byte == -1) {
+	if (read_byte == -1)
+	{
 		error_msg("Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(fd_prev) == -1) {
-		error_msg("Error: Can't close fd %d\n", fd_prev);
-		exit(100);
-	}
-	if (close(fd_new) == -1) {
-		error_msg("Error: Can't close fd %d\n", fd_new);
-		exit(100);
-	}
+	close_fd(fd_prev);
+	close_fd(fd_new);
 	return (0);
 }
