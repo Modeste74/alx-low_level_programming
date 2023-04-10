5c09f3da90e4373981a12c1879e760e6e04a6706
Modeste <ciiramodeste23@gmail.com>
2023-04-10 12:55:05 +0300

    Task done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	size = strlen(text_content);
 	if (text_content != NULL)
 	{
-		written_byte = write(fd, text_content, size);
+		written_byte = write(fd, text_content, size + 1);
 		if (written_byte == -1)
 		{
 			close(fd);
