a8e796724df3ccb015d6c289f4d005cfe497c38c
Modeste Ciira <ciiramodeste23@gmail.com>
2023-05-22 13:03:48 +0300

    Task done

---
+++ 0x18-dynamic_libraries/100-operation.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * add - adds 2 argument given
+ * @a: to be added to arg b
+ * @b to be added to a
+ *
+ * Return: sum of both arguments
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts b for a
+ * @a: to be subtracted from
+ * @b: to subtract from a
+ *
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * div - divides a with b
+ * @a: to be divided
+ * @b: to divide a with
+ *
+ * Return: result after division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mul - return a product of two int arguments
+ * @a: argument 1
+ * @b: argument 2
+ *
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - returns the reminder of 2 argumnets after division
+ * @a: argument 1
+ * @b: argument 2
+ *
+ * Return: reminder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
