889e32532f19e8de6b4db6053ea14cbe2e9c8458
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-26 22:02:28 +0300

    error fixed

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,6 +5,9 @@ question"""
 
 
 def island_perimeter(grid):
+    """function that calculates the
+    of a island given the grid
+    """
     per = 0
     for i in range(0, len(grid)):
         for j in range(0, len(grid[i])):
