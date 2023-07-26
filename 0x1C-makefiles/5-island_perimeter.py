ec956df2c0ca268e48144e0736fcfbd107a534ab
Modeste Ciira <ciiramodeste23@gmail.com>
2023-07-26 21:51:04 +0300

    5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""solution for the
+island perimeter interview
+question"""
+
+
+def island_perimeter(grid):
+    per = 0
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if grid[i][j] == 1:
+                per += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    per -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    per -= 2
+    return per
